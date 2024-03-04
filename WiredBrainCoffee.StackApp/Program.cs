005e0f946fcdcda80970b13aa8d9ff131d91ad3a
ANA-MARIA NICULAE <ana.niculae@ubbcluj.ro>
2024-03-04 11:53:21 +0200

    2.1

--- WiredBrainCoffee.StackApp/Program.cs
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -4,3 +4,16 @@ var stack = new SimpleStack();
 stack.Push(1.2);
 stack.Push(2.2);
 stack.Push(3.2);
+
+Console.WriteLine(stack.Count);
+double sum = 0;
+
+while  (stack.Count > 0)
+{
+    double item = stack.Pop;
+    Console.WriteLine($"Item: {item}" );
+    sum += item;
+}
+
+Console.WriteLine("Sum:" + sum);
+Console.ReadLine();
--- WiredBrainCoffee.StackApp/SimpleStack.cs
+++ WiredBrainCoffee.StackApp/SimpleStack.cs
@@ -2,12 +2,23 @@
 
 public class SimpleStack
 {
-    public SimpleStack()
-    {
-    }
+    private readonly double[] _items;
+    private int _curentIndex = -1;
+    //public SimpleStack()
+    //{
+    //    _items = new double[10];
+    //}
 
-    public void Push(double item)
-    {
-        throw new NotImplementedException();
-    }
+    public SimpleStack() => _items = new double[10]; //experion body
+
+    //public void Push(double item)
+    //{
+    //    _curentIndex++;SS
+    //    _items[_curentIndex] = item;
+    //}
+
+    public int Count => _curentIndex + 1;
+    public void Push(double item) => _items[++_curentIndex] = item;
+
+    public double Pop => _items[_curentIndex--];
 }
